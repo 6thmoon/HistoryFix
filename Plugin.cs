804a6cbf4cfac8123d89eed7e5a3d01a0f03a1c3
6thmoon <59669640+6thmoon@users.noreply.github.com>
2022-10-10 12:50:29 -0700

    Implement interface for manual deletion.
    Expand upon information displayed in the history entry tooltip.

--- Plugin.cs
+++ Plugin.cs
@@ -4,11 +4,15 @@ using HG;
 using RoR2;
 using RoR2.Stats;
 using RoR2.UI;
+using RoR2.UI.LogBook;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Security.Permissions;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 [assembly: AssemblyVersion(Local.Fix.History.Plugin.versionNumber)]
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
@@ -122,6 +126,145 @@ namespace Local.Fix.History
 						ItemCatalog.GetItemDef(example.itemIndex)?.descriptionToken )
 					tooltip.bodyToken = equipment.descriptionToken;
 			}
+		}
+
+		private static readonly Dictionary<Button, RunReport> entries =
+				new Dictionary<Button, RunReport>();
+		private static readonly List<Guid> deletedReports = new List<Guid>();
+
+		[HarmonyPatch(typeof(LogBookController), nameof(LogBookController.BuildEntriesPage))]
+		[HarmonyPrefix]
+		private static void CleanUp() => entries.Clear();
+
+		[HarmonyPatch(typeof(CategoryDef), nameof(CategoryDef.InitializeMorgue))]
+		[HarmonyPostfix]
+		private static void LoadReport(GameObject gameObject, Entry entry)
+		{
+			Button button = gameObject.GetComponent<Button>();
+			if ( button != null && entry?.extraData is RunReport report )
+			{
+				entries[button] = report;
+
+				if ( deletedReports.Contains(report.runGuid) )
+					RemoveButton(button);
+			}
+		}
+
+		[HarmonyPatch(typeof(MPButton), nameof(MPButton.OnPointerClick))]
+		[HarmonyPostfix]
+		private static void OnClick(Button __instance, PointerEventData eventData)
+		{
+			if ( eventData.button != PointerEventData.InputButton.Right ||
+					! entries.TryGetValue(__instance, out RunReport report)
+				) return;
+
+			SimpleDialogBox dialog = SimpleDialogBox.Create();
+
+			dialog.headerToken = new SimpleDialogBox.TokenParamsPair("Delete History Entry");
+			dialog.descriptionToken = new SimpleDialogBox.TokenParamsPair(
+					"Are you sure you want to permanently delete the selected run report?"
+				);
+
+			dialog.AddActionButton(deleteEntry, "DIALOG_OPTION_YES");
+			dialog.AddCancelButton("CANCEL");
+
+			void deleteEntry()
+			{
+				Guid identifier = report.runGuid;
+				MorgueManager.storage.DeleteFile(
+						MorgueManager.historyDirectory / identifier.ToString() + ".xml"
+					);
+
+				deletedReports.Add(identifier);
+				RemoveButton(__instance);
+			}
+		}
+
+		private static void RemoveButton(Button button)
+		{
+			button.enabled = false;
+			button.GetComponentsInChildren<Image>().Do(
+					image => image.enabled = false );
+
+			Color filter = Color.grey.AlphaMultiplied(0.5f);
+
+			button.GetComponentsInChildren<HGTextMeshProUGUI>().Do(
+					text => text.color *= filter );
+			button.GetComponentsInChildren<RawImage>().Do(
+					icon => icon.color *= filter );
+		}
+
+		[HarmonyPatch(typeof(HGButton), nameof(HGButton.OnPointerEnter))]
+		[HarmonyPostfix]
+		private static void UpdateTooltip(HGButton __instance)
+		{
+			LanguageTextMeshController tooltip = __instance.hoverLanguageTextMeshController;
+			if ( tooltip == null ||
+					! entries.TryGetValue(__instance, out RunReport report)
+				) return;
+
+			RuleBook ruleBook = report.ruleBook;
+			RunReport.PlayerInfo player = report.FindFirstPlayerInfo();
+			StatSheet statistics = player.statSheet;
+			DifficultyDef difficulty = DifficultyCatalog.GetDifficultyDef(
+					ruleBook.FindDifficulty()
+				);
+
+			tooltip.token = RemoveSymbols(
+					tooltip.token, Language.GetString("VOIDSURVIVOR_BODY_NAME")
+				) + "\n<style=cSub>" +
+					Language.GetString("LOBBY_CONTROL_PANEL_NAME") +
+						$": " + Language.GetString( report.playerInfoCount > 1 ?
+							"TITLE_MULTIPLAYER" : "TITLE_SINGLEPLAYER" ) + "\n" +
+					Language.GetString("RULE_HEADER_DIFFICULTY") +
+						$": { Language.GetString(difficulty.nameToken) }\n" +
+					Language.GetString("RULE_HEADER_ARTIFACTS") +
+						$": { GetArtifact(report.ruleBook) }\n" +
+					Language.GetString("STATNAME_TOTALTIMEALIVE") +
+						$": { statistics.GetStatDisplayValue(StatDef.totalTimeAlive) }\n" +
+					Language.GetString("STATNAME_TOTALITEMSCOLLECTED").Split().First() +
+						$": { player.itemStacks.Sum() }\n" +
+					Language.GetString("STATNAME_TOTALSTAGESCOMPLETED") +
+						$": { statistics.GetStatValueULong(StatDef.totalStagesCompleted) }\n" +
+				"</style>" +
+				"\n<style=cStack><i>" +
+					"Right-click to delete..." +
+				"</i></style>";
+		}
+
+		private static string RemoveSymbols(string source, string substring)
+		{
+			char[] replacement = substring.Where(
+					character => char.IsLetter(character) || char.IsSeparator(character)
+				).ToArray();
+
+			return source.Replace(substring, new string(replacement));
+		}
+
+		private static string GetArtifact(RuleBook ruleBook)
+		{
+			string artifact = null;
+
+			foreach ( RuleChoiceDef choice in ruleBook.choices )
+			{
+				if ( choice.artifactIndex == ArtifactIndex.None ||
+						 choice.localIndex != ruleBook.GetRuleChoiceIndex(choice.ruleDef)
+					) continue;
+
+				if ( artifact is null )
+				{
+					artifact = ArtifactCatalog.GetArtifactDef(choice.artifactIndex)?.nameToken;
+					if ( artifact is object )
+						artifact = Language.GetString(artifact).Split().Last();
+				}
+				else
+				{
+					artifact = choice.localName;	// Language.GetString("OPTION_ON");
+					break;
+				}
+			}
+
+			return artifact ?? Language.GetString("OPTION_OFF");
 		}
 	}
 }
